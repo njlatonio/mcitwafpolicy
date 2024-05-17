15b9b06a1780237522dea96c13cc46562936e115
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-17 19:33:14 -0400

    Update wafpolicy.tf

--- wafpolicy.tf
+++ wafpolicy.tf
@@ -9,8 +9,8 @@ locals{
 ])
 }
 resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "West Europe"
+  name     = var.resource_name
+  location = var.location
 }
 
 resource "azurerm_web_application_firewall_policy" "example" {
@@ -20,87 +20,87 @@ resource "azurerm_web_application_firewall_policy" "example" {
   location            = azurerm_resource_group.example.location
 
   custom_rules {
-    name      = "Rule1"
-    priority  = 1
-    rule_type = "MatchRule"
+    name      = var.custom_rule_name1
+    priority  = var.custom_rule_prioriy1
+    rule_type = var.custom_rule_type1
 
     match_conditions {
       match_variables {
-        variable_name = "RemoteAddr"
+        variable_name = var.variable_name1
       }
 
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+      operator           = var.operator1
+      negation_condition = var.negation_condition1
+      match_values       = var.match_values1
     }
 
-    action = "Block"
+    action = var.action
   }
 
   custom_rules {
-    name      = "Rule2"
-    priority  = 2
-    rule_type = "MatchRule"
+    name      = var.custom_rule_name2
+    priority  = var.custom_rule_priority2
+    rule_type = var.custom_rule_type2
 
     match_conditions {
       match_variables {
-        variable_name = "RemoteAddr"
+        variable_name = var.variable_name2
       }
 
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24"]
+      operator           = var.operator2
+      negation_condition = var.negation_condition2
+      match_values       = var.match_values2
     }
 
     match_conditions {
       match_variables {
-        variable_name = "RequestHeaders"
-        selector      = "UserAgent"
+        variable_name = var.variable_name3
+        selector      = var.selector
       }
 
-      operator           = "Contains"
-      negation_condition = false
-      match_values       = ["Windows"]
+      operator           = var.operator3
+      negation_condition = var.negation_condition3
+      match_values       = var.match_values3
     }
 
-    action = "Block"
+    action = var.action
   }
 
   policy_settings {
-    enabled                     = true
-    mode                        = "Prevention"
-    request_body_check          = true
-    file_upload_limit_in_mb     = 100
-    max_request_body_size_in_kb = 128
+    enabled                     = var.policy_settings_enable
+    mode                        = var.policy_settings_mode
+    request_body_check          = var.policy_settings_request_body_check
+    file_upload_limit_in_mb     = var.policy_settings_file_upload_limit_in_mb
+    max_request_body_size_in_kb = var.policy_settings_max_request_body_size_in_kb
   }
 
   managed_rules {
     exclusion {
-      match_variable          = "RequestHeaderNames"
-      selector                = "x-company-secret-header"
-      selector_match_operator = "Equals"
+      match_variable          = var.managed_rules_match_variable1
+      selector                = var.managed_rules_selector1
+      selector_match_operator = var.managed_rules_selector_match_operator1
     }
     exclusion {
-      match_variable          = "RequestCookieNames"
-      selector                = "too-tasty"
-      selector_match_operator = "EndsWith"
+      match_variable          = var.managed_rules_match_variable2
+      selector                = var.managed_rules_selector2
+      selector_match_operator = var.managed_rules_selector_match_operator2
     }
 
     managed_rule_set {
-      type    = "OWASP"
-      version = "3.2"
+      type    = var.managed_rule_set_type
+      version = var.managed_rule_set_version
       rule_group_override {
-        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
+        rule_group_name = var.rule_group_name
         rule {
-          id      = "920300"
-          enabled = true
-          action  = "Log"
+          id      = var.rule_id1
+          enabled = var.rule_enabled1
+          action  = var.rule_action1
         }
 
         rule {
-          id      = "920440"
-          enabled = true
-          action  = "Block"
+          id      = var.rule_id2
+          enabled = var.rule_enabled2
+          action  = var.rule_action2
         }
       }
     }
